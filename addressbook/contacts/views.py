263c292b10dc538039d64dc9131c7824f1d128b7
vic <vic@>
2014-01-14 20:14:25 -0500

    first view (homepage) http://127.0.0.1:8000/

--- addressbook/contacts/views.py
+++ addressbook/contacts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from contacts.models import Contact
 
 # Create your views here.
+
+
+class ListContactView(ListView):
+    """
+    View to list contacts
+    """
+
+    model = Contact
+    template_name = 'contact_list.html'
