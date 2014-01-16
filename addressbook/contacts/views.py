f8be67b8f3aaa4795a0dfb024981017acd81a819
vic <vic@>
2014-01-15 20:09:21 -0500

    add a view and two versions of similar tests

--- addressbook/contacts/views.py
+++ addressbook/contacts/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from contacts.models import Contact
 
@@ -12,3 +12,12 @@ class ListContactView(ListView):
 
     model = Contact
     template_name = 'contact_list.html'
+
+
+class CreateContactView(CreateView):
+
+    model = Contact
+    template_name = 'edit_contact.html'
+
+    def get_success_url(self):
+        return reverse('contacts-list')
