1c3ecbf535e292d2d5c43ab3660cd4243282eb24
vic <vic@>
2014-01-16 19:42:45 -0500

    some itegration tests, listed, linked and added

--- addressbook/contacts/tests.py
+++ addressbook/contacts/tests.py
@@ -1,8 +1,55 @@
 from django.test import TestCase
 from django.test.client import Client, RequestFactory
+from django.test import LiveServerTestCase
+
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 from contacts.models import Contact
 from contacts.views import ListContactView
+
+
+class ContactListIntegrationTests(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(ContactListIntegrationTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(ContactListIntegrationTests, cls).tearDownClass()
+
+    def test_contact_listed(self):
+
+        Contact.objects.create(first_name='foo', last_name='bar')
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+
+        self.assertEqual(
+            self.selenium.find_elements_by_css_selector('.contact')[0].text,
+            'foo bar')
+
+    def test_add_contact_linked(self):
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        self.assert_(
+            self.selenium.find_element_by_link_text('add contact'))
+
+    def test_add_contact(self):
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        self.selenium.find_element_by_link_text('add contact').click()
+
+        self.selenium.find_element_by_id('id_first_name').send_keys('test')
+        self.selenium.find_element_by_id('id_last_name').send_keys('contact')
+        self.selenium.find_element_by_id('id_email').send_keys(
+            'test@example.com')
+        self.selenium.find_element_by_id('save_contact').click()
+
+        self.assertEqual(
+            self.selenium.find_elements_by_css_selector('.contact')[-1].text,
+            'test contact')
 
 
 class ContactTests(TestCase):
