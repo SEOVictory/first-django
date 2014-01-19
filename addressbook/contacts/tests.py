63a40116bcc8a099c5bf19eaef9cb81bbf0962e9
vic <vic@>
2014-01-19 10:52:26 -0500

    EditContactFormTests from tutorial using rebar

--- addressbook/contacts/tests.py
+++ addressbook/contacts/tests.py
@@ -5,6 +5,8 @@ from django.core.exceptions import ValidationError, ObjectDoesNotExist
 
 from selenium.webdriver.firefox.webdriver import WebDriver
 
+from rebar.testing import flatten_to_dict
+
 from contacts.models import Contact
 from contacts.views import ListContactView
 from contacts.forms import ContactForm
@@ -244,3 +246,30 @@ class ContactListViewTests(TestCase):
         self.assertEquals(
             response.context_data['object_list'].count(),
             1)
+
+
+class EditContactFormTests(TestCase):
+
+    def test_mismatch_email_is_invalid(self):
+
+        form_data = flatten_to_dict(ContactForm)
+        form_data['first_name'] = 'Foo'
+        form_data['last_name'] = 'Bar'
+        form_data['email'] = 'foo@example.com'
+        form_data['confirm_email'] = 'bar@example.com'
+
+        bound_form = ContactForm(data=form_data)
+
+        self.assertFalse(bound_form.is_valid())
+
+    def test_same_email_is_valid(self):
+
+        form_data = flatten_to_dict(ContactForm)
+        form_data['first_name'] = 'Foo'
+        form_data['last_name'] = 'Bar'
+        form_data['email'] = 'foo@example.com'
+        form_data['confirm_email'] = 'foo@example.com'
+
+        bound_form = ContactForm(data=form_data)
+
+        self.assert_(bound_form.is_valid())
